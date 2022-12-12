666ead6f5ae5384f3da06d88a317a68708fa8d2f
GtDavide01 <davidefiorini01@gmail.com>
2022-12-12 15:02:42 +0100

    aggiunta icona categoria

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-require_once __DIR__ . "/Models/Prodotto.php";
-require_once __DIR__ . "/Models/Gioco.php";
-require_once __DIR__ . "/Models/Cibo.php";
-require_once __DIR__ . "/Models/Cuccia.php";
-
-$item1 = new Prodotto("Corda", 14.99, "gg.pjg", new Categoria("Cani"));
-$items = [
-    new Cibo("Crocchete per cani", 15.49, "https://www.ideashoppingcenter.it/files/archivio_Files/Foto/44645_2.JPG", new Categoria("Cani"), "1 kg", "Alimenti secchi per cani", "1 kg di crocchette bilanciate per cani Wolf of Wilderness, senza cereali né glutine, ispirate all'alimentazione originaria del lupo in natura"),
-    new Gioco("Gioco per gatti  corda con due palline", 8.19, "https://ss-pics.s3.eu-west-1.amazonaws.com/files/2056905/page-set-di-palline-con-coda-e-sonaglio-in-plastica.jpg?1636994304", new Categoria("gatti"), "Plastica", "Da mordere", "Gioco dai colori festosi per gatti, unisce una corda colorata in poliestere a 2 palline in TPR con punte"),
-    new Cuccia("Letto Trixie Elika", 28.99, "https://shop-cdn-m.mediazs.com/bilder/letto/batumi/3/400/1_203335_geop_zooplusexclusive_katzenbett_batumi_hs_04_3.jpg", new Categoria("gatti"), "1/2 kg", "50cm circonferenza", "Letto per gatti, cuccioli e cani di piccola taglia, in morbido peluche, confortevole bordo alto di facile accesso"),
-    new Cibo("Crocchete per cani", 15.49, "https://www.ideashoppingcenter.it/files/archivio_Files/Foto/44645_2.JPG", new Categoria("Cani"), "1 kg", "Alimenti secchi per cani", "1 kg di crocchette bilanciate per cani Wolf of Wilderness, senza cereali né glutine, ispirate all'alimentazione originaria del lupo in natura"),
-    new Gioco("Gioco per gatti  corda con due palline", 8.19, "https://ss-pics.s3.eu-west-1.amazonaws.com/files/2056905/page-set-di-palline-con-coda-e-sonaglio-in-plastica.jpg?1636994304", new Categoria("gatti"), "Plastica", "Da mordere", "Gioco dai colori festosi per gatti, unisce una corda colorata in poliestere a 2 palline in TPR con punte"),
-    new Cuccia("Letto Trixie Elika", 28.99, "https://shop-cdn-m.mediazs.com/bilder/letto/batumi/3/400/1_203335_geop_zooplusexclusive_katzenbett_batumi_hs_04_3.jpg", new Categoria("gatti"), "1/2 kg", "50cm circonferenza", "Letto per gatti, cuccioli e cani di piccola taglia, in morbido peluche, confortevole bordo alto di facile accesso")
-];
-// var_dump($items);
-?>
 <!-- Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
  - L'e-commerce vende **prodotti** per animali. I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.
  - I prodotti saranno oltre al **cibo**, anche **giochi**, **cucce**, etc.
@@ -24,6 +7,14 @@ BONUS (Opzionale):
  Il cliente effettua il pagamento dei prodotti nel **carrello** con la **carta di credito**, che non deve essere scaduta.
  Fare refactoring -->
 
+<?php
+require_once __DIR__ . "/Models/Prodotto.php";
+require_once __DIR__ . "/Models/Gioco.php";
+require_once __DIR__ . "/Models/Cibo.php";
+require_once __DIR__ . "/Models/Cuccia.php";
+require_once __DIR__ . "/Models/Database.php";
+// var_dump($items);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -35,6 +26,8 @@ BONUS (Opzionale):
     <link rel="stylesheet" href="css/style.css">
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>PHP-OOP-2</title>
 </head>
 
@@ -53,7 +46,15 @@ BONUS (Opzionale):
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item"> Prezzo : <?php echo  $item->price ?></li>
-                        <li class="list-group-item"> Categoria : <?php echo  $item->category->name ?></li>
+                        <li class="list-group-item">
+                            <?php if ($item->category->name ==  "Cani") {
+                            ?> <i class="fa-solid fa-dog"></i> <?php echo "Cani";
+                                                            } else {
+                                                                ?>
+                                <i class="fa-solid fa-cat"></i>
+                            <?php echo "Gatti";
+                                                            } ?>
+                        </li>
                         <li class="list-group-item"> Materiale :
                             <?php if ($item->material) {
                                 echo $item->material;
